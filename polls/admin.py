c7bc03dca79ba1dc8e64bba3921663c1fa8555a2
zhupeijun <zhupeijun2006@163.com>
2022-03-08 00:55:23 +0900

    custom admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,26 @@
+from dataclasses import field
+from pyexpat import model
 from django.contrib import admin
 
 # Register your models here.
 from polls.models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
